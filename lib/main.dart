6f7f6125df26c8c39eb4e0a71bda1c2bf91b253f
rafamenegheti <rafaelmenegheti52@gmail.com>
2021-11-02 21:34:55 -0300

    11/02/2021

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+//import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+import 'modules/home/home_page.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PayFlow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
